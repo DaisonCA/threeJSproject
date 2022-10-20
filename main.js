9b1977ccd4dfe91e97b486f157689e4a4761bb95
Daison <test@text.to>
2022-10-20 20:48:51 +0530

    uploading the entire project onto the remote branch

---
+++ main.js
@@ -0,0 +1,103 @@
+import './style.css'
+import * as THREE from 'three'
+import { Camera, MathUtils } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16 ,100);
+const material = new THREE.MeshStandardMaterial({ color: 0x4169E1 });
+const torus = new THREE.Mesh(geometry, material);
+
+//lighting
+const pointLight = new THREE.PointLight(0xffffff);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+//Helpers : help to locate position
+const pointLightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(100, 20);
+
+pointLight.position.set(5,5,5);
+scene.add(pointLight, ambientLight);
+scene.add(pointLightHelper, gridHelper);
+scene.add(torus);
+
+//adding orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.001;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.001;
+  controls.update();               //orbit controls
+  renderer.render(scene, camera);
+}
+animate();
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25,24,24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x,y,z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+//fill(): fills/replaces each position of a given array with specified values
+//forEach(): call a function() for element in a given array
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+//cat
+const catTexture = new THREE.TextureLoader().load('cat.jpg');
+const cat = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshStandardMaterial({ map: catTexture })
+);
+scene.add(cat);
+
+//moon
+const moonImg = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normalMapMoon.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial({
+    map: moonImg,
+    normalMap: normalTexture
+  })
+);
+
+moon.position.setX(5);
+scene.add(moon);
+
+//on scrolling
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  moon.rotation.x += 0.01;
+  moon.rotation.y += 0.01;
+  moon.rotation.z += 0.01;
+
+  cat.rotation.y += 0.01;
+  cat.rotation.x += 0.01;
+
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+  camera.position.z = t * -0.01;
+}
+
+document.body.onscroll = moveCamera;
